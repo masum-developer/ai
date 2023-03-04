6cc4e3a580db29c2add1b98b53bbd8c919d159aa
masumbillahwebdeveloper <masumbillah.webdeveloper@gmail.com>
2023-03-04 21:15:39 +0600

    ternary validation done

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,6 @@
 let b=[];
+
+/*Show sort data*/
 const showData= (id, name, image,features,published_in,div )=>{
     div.classList.add('col');
 let l='';
@@ -7,11 +9,12 @@ const items=features.map(p=>{
     
 });
 div.innerHTML+=`<div class="card h-50">
-<img src="${image}" class="card-img-top img-fluid h-75 rounded-start" alt="...">
+
 <div>
+<img src="${image}" class="img-fluid h-75" alt="...">
  <div style="height:100px;">
-  <h5>Features</h5>
-  <ol id="list-container" class="list-group list-group-numbered">`
+  <p>Features</p>
+  <ol id="list-container" class="p-2 ms-2">`
   div.innerHTML+=l;
   div.innerHTML+=`
   </ol>
@@ -25,21 +28,23 @@ div.innerHTML+=`<div class="card h-50">
 <h2 class="m-3">${name}</h2>
 <small class="text-muted"><i class="fa-regular fa-calendar-days m-3"></i>${published_in}</small>
 </div>
-<div>
-<a class="text-danger me-4 mt-5" data-bs-toggle="modal" data-bs-target="#aiModal" onclick="loadAiDetails('${id}')"><i class="fa-solid fa-circle-arrow-right"></i></a>
+<div style="margin-left:100px;">
+<a class="text-danger me-4 mt-5" data-bs-toggle="modal" data-bs-target="#aiModal" onclick="loadAiDetails('${id}')"><i style="font-size:30px;" class="fa-solid fa-circle-arrow-right"></i></a>
 </div>
 </div>
 </div>
 </div>
 `
 }
+
 /*  Display Ai details* */
 const displayAiDetails = data=>{
+    console.log(data);
     const { id,description,features,image_link,integrations,input_output_examples,pricing,accuracy } = data;
-    //console.log(integrations);
-    //console.log(data);
-    // document.getElementById('description-id').innerText=data.data.description;
-    document.getElementById('modal-container').innerHTML=`
+    //console.log(pricing);
+    
+    document.getElementById('modal-container').textContent='';
+    document.getElementById('modal-container').innerHTML+=`
         <div class="w-100 bg-danger-subtle border-danger p-4">
             <div>
                 <p id="description-id">${description}</p>
@@ -47,36 +52,37 @@ const displayAiDetails = data=>{
             <div class="d-flex justify-content-between g-4 mt-3">
                 <div class="card p-2" style"width:70px;">
                     <div>
-                      <p">${pricing[0].price==0?'Free of cost':pricing[0].price}   ${pricing[0].plan}</p>
+                      <p">${(pricing===null)||(pricing[0].price==='0') ?'Free of cost':pricing[0].price}   ${pricing===null?'Free of cost':pricing[0].plan}</p>
+                      
                     </div>
                 </div>
                 <div class="card p-2" style"width:43px;">
                     <div>
-                    <p">${pricing[1].price+' '+ pricing[1].plan}</p>
+                    
+                    <p">${pricing===null?'Free of cost':pricing[1].price}   ${pricing===null?'Free of cost':pricing[1].plan}</p>
                     </div>
                 </div>
                 <div class="card p-2" style"width:43px;">
                     <div>
-                    <p">${pricing[2].price+' '+ pricing[2].plan}</p>
+                    <p">${pricing===null ?'Free of cost':pricing[2].price}   ${pricing===null ?'Free of cost':pricing[2].plan}</p>
                     </div>
                 </div>
             </div>
             <div class="d-flex justify-content-between mt-3">
                 <div>
                     <h3>Features</h3>
-                    <ul>
-                        <li>${features[1].feature_name}</li>
-                        <li>${features[2].feature_name}</li>
-                        <li>${features[3].feature_name}</li>
-                    </ul>
+                    
+                        <p>${features[1].feature_name}</p>
+                        <p>${features[2].feature_name}</p>
+                        <p>${features[3].feature_name}</p>
+                        <p>${features[4]?features[4].feature_name:''}</p>
+                    
                 </div>
                 <div>
                     <h3>Integrations</h3>
-                    <ul>
-                        <li>${integrations[0]?integrations[0]:'No data found'}</li>
-                        <li>${integrations[1]}</li>
-                        <li>${integrations[2]}</li>
-                    </ul>
+                       <p>${integrations===null||(integrations[0]===undefined)?'No data found':integrations[0]}</p>
+                        <p>${(integrations===null)||(integrations[1]===undefined)?'':integrations[1]}</p>
+                        <p>${(integrations===null)||(integrations[2]===undefined)?'':integrations[2]}</p>
                 </div>
             </div>
         </div>
@@ -85,14 +91,15 @@ const displayAiDetails = data=>{
             <img class="img-fluid rounded-3 position-relative" src="${image_link[0]}" id="image-id" alt="">
             <span class="badge text-bg-danger position-absolute top-0 end-0 mt-5 me-5">${accuracy.score?accuracy.score*100+'% accuracy':''}</span>
             </div>
-            <h5 class="text-center">${input_output_examples[0].input}</h5>
-            <p class="text-center">${input_output_examples[0].output?input_output_examples[0].output:''}</p>
+            <h5 class="text-center">${input_output_examples===null ? 'can you give me any example':input_output_examples[0].input}</h5>
+            <p class="text-center">${input_output_examples===null ?'No! Not Yet! Take a break!!!':input_output_examples[0].output}</p>
         </div>
     `
 }
+/*..................Display Ai Details End.....................*/
 let aiData;
 
-/*........................................*/
+/*.................Load Ai Details Data.......................*/
 const loadAiDetails = async id =>{
     const URL=`https://openapi.programming-hero.com/api/ai/tool/${id}`
     const res = await fetch(URL);
@@ -100,19 +107,16 @@ const loadAiDetails = async id =>{
     displayAiDetails(data.data);
 }
 
-
-
 /*......display 6 data.....*/
 const loadAiData =async ()=>{
+    document.getElementById('loader').classList.remove('d-none');
     const URL ="https://openapi.programming-hero.com/api/ai/tools";
     const res = await fetch(URL);
     const data= await res.json();
     aiData=data.data.tools;
-  //  console.log(aiData);
-    document.getElementById('loader').classList.remove('d-none');
+    
     displayData(aiData,6);
 }
-
 
 const displayData = (data,dataLimit)=>{
     b=[];
@@ -128,11 +132,7 @@ const displayData = (data,dataLimit)=>{
 //    let sliceData=data.slice(0,6);
    sliceData.forEach(element => {
         const { id, name, image,features,published_in } = element;
-        //console.log(published_in);
-        
 /*.......................date format start........................*/
-
-
 function dateSort(text,idNumber){
   const myArray = text.split("/");
   let newDate=myArray[2]+'-0'+myArray[1]+'-'+myArray[0];
@@ -150,21 +150,24 @@ dateSort(published_in,id);
 
 /*.......................date format end........................*/
 let div= document.createElement('div');
-
+document.getElementById('loader').classList.add('d-none');
 showData(id, name, image,features,published_in ,div);
         cardContainer.appendChild(div);   
-        
     });
- document.getElementById('loader').classList.add('d-none');
-     
 }
+
+/*.......................Show All Button........................*/
 document.getElementById('show-all').addEventListener('click',function(){
     document.getElementById('btn-section').classList.add('d-none');
     document.getElementById('loader').classList.remove('d-none');
     displayData(aiData,7);
 })   
+
+/*.......................Call Load Ai Data........................*/
 loadAiData();
 console.log(b);
+
+/*..............Sort By Date.................*/
 document.getElementById('sort-date-id').addEventListener('click',function(){
     const my = b.map(obj => {
         return {...obj, date: new Date(obj.date)};
@@ -178,9 +181,9 @@ document.getElementById('sort-date-id').addEventListener('click',function(){
    cardContainer.textContent='';
     sortedDesc.forEach(element=>{
         const { id, name, image,features,published_in } = element;
-        
-   /*...............................*/
+         
    let div= document.createElement('div');
+   /*.............Call Show sort data..................*/
    showData(id, name, image,features,published_in,div );
    
         cardContainer.appendChild(div);
